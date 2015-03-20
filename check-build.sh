f00e90ee0690a2b76e5bfd3f7b1664aa3939c0b5
Bruce Becker <brucellino@gmail.com>
2015-03-20 14:59:08 +0100

    Added module to check-build

--- check-build.sh
+++ check-build.sh
@@ -1,33 +1,37 @@
 module load ci
-echo "About to make teh "
+echo "About to make the modules"
 cd $WORKSPACE/$NAME
 ls
 echo $?
 
-#make install # DESTDIR=$SOFT_DIR
+make install # DESTDIR=$SOFT_DIR
 
 #mkdir -p $REPO_DIR
 #rm -rf $REPO_DIR/*
 #tar -cvzf $REPO_DIR/build.tar.gz -C $WORKSPACE/build apprepo
 #
-# mkdir -p modules
-# (
-# cat <<MODULE_FILE
-# #%Module1.0
-# ## $NAME modulefile
-# ##
-# proc ModulesHelp { } {
-#     puts stderr "       This module does nothing but alert the user"
-#     puts stderr "       that the [module-info name] module is not available"
-# }
-#
-# module-whatis   "$NAME $VERSION."
-# setenv       GMP_VERSION       $VERSION
-# setenv       GMP_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
-# prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
-# prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
-# MODULE_FILE
-# ) > modules/$VERSION
-#
-# mkdir -p $LIBRARIES_MODULES/$NAME
-# cp modules/$VERSION $LIBRARIES_MODULES/$NAME
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+   puts stderr "       This module does nothing but alert the user"
+   puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       QE_VERSION       $VERSION
+setenv       QE_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path PATH              $::env(QE_DIR)/bin
+prepend-path LD_LIBRARY_PATH   $::env(QE_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(QE_DIR)/include
+MODULE_FILE
+) > modules/$VERSION
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION $LIBRARIES_MODULES/$NAME
+
+module avail
+module list
+module add $NAME
