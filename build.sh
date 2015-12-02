d633908191f91dfbc4266781307c99bcb2861bf7
Bruce Becker <brucellino@gmail.com>
2015-12-02 08:10:37 +0100

    Using WORKSPACE instead of SRC_DIR for the pull location

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ SOURCE_REPO="https://github.com/QEF/q-e.git"
 # We pretend that the $SOURCE_FILE is there, even though it's actually a dir.
 SOURCE_FILE=${NAME}
 module load ci
-#module load gcc/4.8.2
-#module add fftw
+
 
 echo "REPO_DIR is ${REPO_DIR}"
 echo "SRC_DIR is ${SRC_DIR}"
@@ -22,28 +21,29 @@ mkdir -p ${SOFT_DIR}
 
 #  Download the source file
 # since it's from SCM, should be done in ${WORKSPACE} instead of ${SRC_DIR}, but whatever
-if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
-  touch  ${SRC_DIR}/${SOURCE_FILE}.lock
-  echo "seems like this is the first build - doing a git clone  https://github.com/SouthAfricaDigitalScience/q-e.git ${SRC_DIR}/${SOURCE_FILE}"
-  mkdir -p ${SRC_DIR}
-  git clone https://github.com/SouthAfricaDigitalScience/q-e.git ${SRC_DIR}/${SOURCE_FILE}
+if [ ! -e ${WORKSPACE}/${SOURCE_FILE}.lock ] && [ ! -s ${WORKSPACE}/${SOURCE_FILE} ] ; then
+# First download
+  touch  ${WORKSPACE}/${SOURCE_FILE}.lock
+  echo "seems like this is the first build - doing a git clone  https://github.com/SouthAfricaDigitalScience/q-e.git to ${WORKSPACE}/${SOURCE_FILE}"
+  git clone https://github.com/SouthAfricaDigitalScience/q-e.git ${WORKSPACE}/${SOURCE_FILE}
   echo "releasing lock"
-  rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
-elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
-  # Someone else has the file, wait till it's released
-  while [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; do
+  rm -v ${WORKSPACE}/${SOURCE_FILE}.lock
+elif [ -e ${WORKSPACE}/${SOURCE_FILE}.lock ] ; then
+# Someone else has the file, wait till it's released
+  while [ -e ${WORKSPACE}/${SOURCE_FILE}.lock ] ; do
     echo " There seems to be a download currently under way, will check again in 5 sec"
     sleep 5
   done
 else
-  echo "continuing from previous builds, using source at " ${SRC_DIR}/${SOURCE_FILE}
-  cd ${SRC_DIR}/${SOURCE_FILE}
+  echo "continuing from previous builds, using source at " ${WORKSPACE}/${SOURCE_FILE}
+  cd ${WORKSPACE}/${SOURCE_FILE}
+  echo "claiming the pull"
+  touch  ${WORKSPACE}/${SOURCE_FILE}.lock
   echo "Doing a git pull..."
   git pull
+  echo "releasing lock"
+  rm -v ${WORKSPACE}/{SOURCE_FILE}.lock
 fi
-# this is the equivalent of a git pull :
-# tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
-cp -rf ${SRC_DIR}/${SOURCE_FILE} ${WORKSPACE} --no-clobber
 cd ${WORKSPACE}/${NAME}
 echo "cleaning up previous builds"
 make distclean
